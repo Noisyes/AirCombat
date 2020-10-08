1a3f0ddcad6fa776b910b247525309219f92664c
noisyes <kyf19970408@163.com>
2020-10-09 00:27:42 +0800

    add litJson

---
+++ Assets/Scripts/Module/Config/IReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IReader
+{
+    IReader this[string key] { get; }
+    IReader this[int key] { get; }
+    void Get<T>(Action<T> callBack);
+    void SetData(object data);
+}
---
+++ Assets/Scripts/Module/Config/JsonReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+
+public class JsonReader : IReader
+{
+    private JsonData _data;
+    private JsonData _curData;
+    public IReader this[string key]
+    {
+        get
+        {
+            _curData = _data[key];
+            return this;
+        }
+    }
+
+    public IReader this[int key]
+    {
+        get
+        {
+            _curData = _curData[key];
+            return this;
+        }
+    }
+
+    public void Get<T>(Action<T> callBack)
+    {
+        T data = GetValue<T>(_curData);
+        callBack?.Invoke(data);
+        ResetData();
+    }
+
+    private T GetValue<T>(JsonData data)
+    {
+        
+    }
+
+    private void ResetData()
+    {
+        _curData = _data;
+    }
+
+    public void SetData(object data)
+    {
+        if (data is string)
+        {
+            _data = JsonMapper.ToObject(data as string);
+            ResetData();
+        }
+        else
+        {
+            Debug.LogError("不是Json类型的数据");
+        }
+    }
+}
